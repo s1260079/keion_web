1dad08d050ee3863acfc5626a2483acb8219d3ae
s1260079 <when.rains.pours@gmail.com>
2021-07-09 12:32:00 +0900

    Update:UserSessionsControllerの機能を修正しました。

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,17 @@
-class UserSessionsController < ApplicationController
-  def new
+class  UserSessionsController < ApplicationController
+  def  create
+    @user  =  login （params [ ：email ] 、 params [ ：password ] ）
+
+    もし @userの
+      redirect_back_or_to （：ユーザー、 予告：'ログイン成功' ）
+    他の
+      フラッシュ。now [ ：alert ]  =  'ログインに失敗しました'
+      render  action：'new'
+    end
   end
 
-  def create
-  end
-
-  def destroy
+  def  destroy
+    logout
+    redirect_to （：users 、 notice：'ログアウトしました！' ）
   end
 end
