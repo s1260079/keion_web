babdcf0f208fa1e30b02bf31065f93b826815802
s1260079 <when.rains.pours@gmail.com>
2021-07-10 06:52:40 +0900

    Fix:ロケットハッシュからシンボルに修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root :to => 'users#index'
+  root to: 'users#index'
   resources :users
 
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => "user_sessions#create"
-  post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login', to: 'user_sessions#new', as: 'login'
+  post 'login', to: "user_sessions#create"
+  post 'logout', to: 'user_sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
