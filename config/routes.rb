34571abe085d601b858486d0dd7f2d3b7aae6a36
s1260079 <when.rains.pours@gmail.com>
2021-07-09 12:36:37 +0900

    Update:ルーティングに変更を加えました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root :to => 'users#index'
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
