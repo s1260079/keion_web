3ef5a4db7aca6d7df414a17e561be3ca5fa8f271
s1260079 <when.rains.pours@gmail.com>
2021-07-09 12:50:18 +0900

    Add:Userテーブルに新たなカラムを追加するために新しいmigrationファイルを生成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_025903) do
+ActiveRecord::Schema.define(version: 2021_07_09_034734) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2021_07_09_025903) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "studentID"
+    t.string "part"
+    t.text "likeBand"
+    t.text "message"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
