2dad69798b3996adec179db354ef364330b78380
KouSei089 <qq00891829@icloud.com>
2021-07-10 09:03:01 +0900

    Add:コントローラ作成時不要なファイルが生成されないよう設定しました

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Rails5Cuo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.skip_routes true # trueの時routes.rbを変更しない
+      g.helper false # helperファイル生成しない
+      g.test_framework false # testファイル生成しない
+      g.assets false # stylesheets, jsファイル生成を生成しない
+     end
   end
 end
