6d2d977e649c37da622781e608eccd8330727941
s1260079 <when.rains.pours@gmail.com>
2021-07-13 09:06:25 +0900

    Add:バンドページを作成しました。

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,7 +1,8 @@
 class BandsController < ApplicationController
+  before_action :set_band, only: [:show, :edit, :update, :destroy]
   def index
+    @bands=Band.all
   end
-
   def new
   end
 
@@ -10,4 +11,14 @@ class BandsController < ApplicationController
 
   def show
   end
+
+  private
+  def set_band
+    @band = Band.find(params[:id])
+  end
+  def band_params
+    # byebug
+    params.require(:band).permit(:band_name, :band_members, :band_message)
+  end
+
 end
