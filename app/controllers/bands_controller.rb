bf6f0007cd6f9c29f740917b223e9a679e59e3f1
s1260079 <when.rains.pours@gmail.com>
2021-07-15 22:23:53 +0900

    Fix:コントローラのインデントとスペースを修正しました。

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,27 +1,25 @@
 class BandsController < ApplicationController
   protect_from_forgery
   before_action :set_band, only: [:show, :edit, :update, :destroy]
+
   def index
     @bands=Band.all
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
-
     @band=Band.new
   end
 
   def create
     @band = Band.new(band_params)
-    if @band.save
-      redirect_to(:bands, notice: 'Success!')
-    else
-      flash[:alert] = 'Save error!'
-      render :new
-    end
+      if @band.save
+        redirect_to(:bands, notice: 'Success!')
+      else
+        flash[:alert] = 'Save error!'
+        render :new
+      end
   end
 
   def edit
@@ -30,28 +28,28 @@ class BandsController < ApplicationController
 
   def update
     @band = Band.find(params[:id])
-    if @band.update(band_params)
-      redirect_to(:bands, notice: 'Success!')
-    else
-      flash[:alert] = 'Save error!'
-      render :edit
-    end
+      if @band.update(band_params)
+        redirect_to(:bands, notice: 'Success!')
+      else
+         flash[:alert] = 'Save error!'
+        render :edit
+      end
   end
 
   def destroy
-
     @band.destroy
     redirect_to(:bands, notice: 'Success!')
   end
 
 
   private
-  def set_band
-    @band = Band.find(params[:id])
-  end
-  def band_params
-    # byebug
-    params.permit(:band_name, :band_members, :band_message)
-  end
+
+    def set_band
+      @band = Band.find(params[:id])
+    end
+
+    def band_params
+      params.permit(:band_name, :band_members, :band_message)
+    end
 
 end
