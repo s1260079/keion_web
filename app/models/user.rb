03d86fd6c7e0146da0c10b01c819febae4380c66
s1260079 <when.rains.pours@gmail.com>
2021-07-31 08:19:15 +0900

    Update:バリデーションを追記しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  validates :user_name, :student_id, presence: true
+
+  validates :student_id, presence: true,
+            format: { with:  /\A^[a-z0-9]+\z/, message: "ID should consist of lowercase letters and numbers(ex: s1300350)" }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
 
-  validates :user_name, :student_id, presence: true
 
   # def division
   #   for student in user do
