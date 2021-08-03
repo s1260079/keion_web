726107e50219cfd10ddde4b29f53196a1002e57b
s1260079 <when.rains.pours@gmail.com>
2021-08-03 18:56:15 +0900

    Fix:bandん編集ができるようにしました。

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  def default_url
+    'no_image.png'
+  end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
