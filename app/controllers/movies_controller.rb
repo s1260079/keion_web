02c44aeb1499c267eb6dab535c9b7f67dcbbe147
s1260079 <when.rains.pours@gmail.com>
2021-08-03 23:26:39 +0900

    Update:表示を降順になるようにしました。

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   before_action :set_movie, only: %i[show edit update destroy]
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all.order("id DESC")
   end
 
   def show; end
