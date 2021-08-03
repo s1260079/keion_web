795dabeb8ead1e71dc845e38c87d0fe97e8cb0f4
s1260079 <when.rains.pours@gmail.com>
2021-08-03 19:05:57 +0900

    Add:moviesのルートを設定しました。

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :users
   resources :bands
+  resources :movies
 
   get 'login', to: 'user_sessions#new', as: 'login'
   post 'login', to: 'user_sessions#create'
