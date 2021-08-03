8f52d849a474462181085778e7937ec505c3d677
s1260079 <when.rains.pours@gmail.com>
2021-08-03 19:04:40 +0900

    Add:movieテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_233016) do
+ActiveRecord::Schema.define(version: 2021_08_03_100317) do
 
   create_table "bands", force: :cascade do |t|
     t.string "band_name", null: false
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2021_07_31_233016) do
     t.integer "user_id"
     t.string "image_id"
     t.string "avatar_path"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
