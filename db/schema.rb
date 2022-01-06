1e1f1c1ab3519953f7e9b495a7315234088b679c
s1260079 <when.rains.pours@gmail.com>
2022-01-06 17:43:55 +0900

    Add:マイグレーションファイルの最適化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_083325) do
+ActiveRecord::Schema.define(version: 2022_01_06_084055) do
 
   create_table "bands", force: :cascade do |t|
     t.string "band_name", null: false
